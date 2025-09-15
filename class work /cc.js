206ebb4128cb53fa91762c1b86f9d30cffd5564e
Ibrokhim Ushurov <ibrokhimushurov@usf.edu>
2025-09-14 22:19:18 -0400

    Creating Customer Records

---
+++ class work /cc.js
@@ -0,0 +1,2 @@
+let foods = ["hot dog", "tacos", "burger", "nachos"]
+foods.forEach( food => console.log('I want to order s(foods),')
---
+++ customer-tracker/cc_3a.js
@@ -0,0 +1,18 @@
+// Step 2: Create Customer Records
+let Customers = [
+    {
+        name: "Kate Johnson", 
+        email: "Jkate@example.com",
+        purchases: ["Charger", "Iphone case",]
+    }, 
+    {
+        name: "Lara Lee", 
+        email: "Lare.Lee@example.com",
+        purchases: ["Washer Combo", "Microwave", "TV"]
+    },
+    {
+        name: "Liam Jolie", 
+        email: "Jolie.Liam@example.com",
+        purchases: ["Tier", "Rear Window", "Security Camera"]
+    }
+];
